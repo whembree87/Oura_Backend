e6039cb926f6f387077ed66040587fd90ff47644
whembree87 <whembree87@gmail.com>
2024-03-12 16:46:33 -0700

    Add repo manager interface for KED Heavy

---
+++ src/main/java/com/oura/backend/repo_manager/IKedHeavyRepoManager.java
@@ -0,0 +1,9 @@
+package com.oura.backend.repo_manager;
+
+import com.oura.backend.entity.KedHeavyEntity;
+
+import java.util.List;
+
+public interface IKedHeavyRepoManager {
+    List<KedHeavyEntity> getKedHeavy();
+}
