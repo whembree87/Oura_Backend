3979ce83ae6f1c34afc2537ecb526a5c6d6494b4
whembree87 <whembree87@gmail.com>
2024-05-28 18:00:15 -0700

    Add KED Heavy Count model

---
+++ src/main/java/com/oura/backend/model/KedHeavyCount.java
@@ -0,0 +1,14 @@
+package com.oura.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class KedHeavyCount {
+    long count;
+}
